a931f6711cf2f27dec336fe95beeff35682cb99f
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-28 20:21:12 -0700

    se agrego una prueba con la se comprueba el filtrado de alumnos por creditos del campusServices

--- test/services/campusServices.test.js
+++ test/services/campusServices.test.js
@@ -28,4 +28,33 @@ describe("Test para campusServices", () => {
         const emailWithCertification = campusServices.getStudentsEmailsByCertification(students)
         expect(emailWithCertification[0]).toMatch(/Beulah@visualpartnership.xyz/);
     });
+
+    test('2. obtain student name by credits > 500', () => {
+        const students = [   {
+            "id": "6264d5d81872b70bb0c171a6",
+            "name": "Monroe",
+            "email": "Beulah@visualpartnership.xyz",
+            "credits": 329,
+            "enrollments": [
+                "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 8,
+            "haveCertification": true
+        },
+        {
+            "id": "6264d5d85659057b4331973b",
+            "name": "Chase",
+            "email": "Moses@visualpartnership.xyz",
+            "credits": 769,
+            "enrollments": [
+                "Visual Thinking Avanzado",
+                "Visual Thinking Intermedio"
+            ],
+            "previousCourses": 3,
+            "haveCertification": false
+        }];
+        const creditsGreaterThan500 = campusServices.getStudentsByCredits(students)
+        expect(creditsGreaterThan500[0]).toMatch(/Chase/);
+    })
+
 });
