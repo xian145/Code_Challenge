388bdb2fefa2ec3a2cdab74f3a8d1cbad0c31c02
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-28 20:32:53 -0700

    se elimino un archivo duplicado

--- test/campusController.test.js
+++
@@ -1,8 +0,0 @@
-const campusController = require('../lib/controllers/campusController')
-
-describe("Test para campusController", () => {
-    test("1. Obtain list of students", () => {
-        const students = campusController.getStudents();
-        expect(students).toHaveLength(51);
-    });
-});
