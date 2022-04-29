07a81ff11615892abaa1fec384042a25526cda13
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-28 19:17:25 -0700

    se creo prueba para comprobar que studentServices.getStudentsEmailsByCertification devuelva el email de los estudiantes que tienen certificacion

---
+++ test/services/campusServices.test.js
@@ -0,0 +1,31 @@
+const campusServices = require('../../lib/services/campusServices')
+
+describe("Test para campusServices", () => {
+    test("1. Obtain emails of students with certification", () => {
+        const students = [   {
+            "id": "6264d5d81872b70bb0c171a6",
+            "name": "Monroe",
+            "email": "Beulah@visualpartnership.xyz",
+            "credits": 329,
+            "enrollments": [
+                "Visual Thinking Avanzado"
+            ],
+            "previousCourses": 8,
+            "haveCertification": true
+        },
+        {
+            "id": "6264d5d85659057b4331973b",
+            "name": "Chase",
+            "email": "Moses@visualpartnership.xyz",
+            "credits": 769,
+            "enrollments": [
+                "Visual Thinking Avanzado",
+                "Visual Thinking Intermedio"
+            ],
+            "previousCourses": 3,
+            "haveCertification": false
+        }];
+        const emailWithCertification = campusServices.getStudentsEmailsByCertification(students)
+        expect(emailWithCertification[0]).toMatch(/Beulah@visualpartnership.xyz/);
+    });
+});
